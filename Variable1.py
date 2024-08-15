ee89cd5b3d9194600b86332e66f928bf59ab273e
Corbin McDougal <mcdoucor000@warren.k12.in.us>
2024-08-15 08:39:17 -0400

    initial commit

---
+++ Variable1.py
@@ -0,0 +1,8 @@
+Number_of_apples =  20
+Number_of_oranges = 15
+print("Number of Apples:",Number_of_apples)
+print("Number of Oranges:",Number_of_oranges)
+Number_of_apples = 0
+Number_of_oranges = 0
+print("Number of Apples:",Number_of_apples)
+print("Number of Oranges:",Number_of_oranges)
